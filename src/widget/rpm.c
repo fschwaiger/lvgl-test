d15f1755fb66937fcf887072def9977c2cbbf15c
Florian Schwaiger <fschwaiger@gmail.com>
2026-01-07 23:50:03 +0100

    wip

--- src/widget/rpm.c
+++ src/widget/rpm.c
@@ -1,22 +1,28 @@
 #include "rpm.h"
 
+lv_color_t WHITE = {.red = 255, .green = 255, .blue = 255};
+lv_color_t AMBER = {.red = 255, .green = 221, .blue = 0};
+lv_color_t RED = {.red = 255, .green = 0, .blue = 0};
+
 void av_bind_color(lv_observer_t *observer, lv_subject_t *subject)
 {
-    // if above 1500 rpm, set arc color to red, else to white
     lv_obj_t *arc = (lv_obj_t *)lv_observer_get_user_data(observer);
-    float rpm_value = lv_subject_get_float(subject);
-    if (rpm_value > 1750.0f)
-    {
-        lv_obj_set_style_arc_color(arc, lv_color_hex3(0xF00), LV_PART_INDICATOR);
-    }
-    else if (rpm_value > 1500.0f)
-    {
-        lv_obj_set_style_arc_color(arc, lv_color_hex3(0xFD0), LV_PART_INDICATOR);
-    }
-    else
-    {
-        lv_obj_set_style_arc_color(arc, lv_color_hex3(0xAAA), LV_PART_INDICATOR);
-    }
+    float v = lv_subject_get_float(subject);
+    lv_color_t c = (v > 1750) ? RED : (v > 1500) ? AMBER
+                                                 : WHITE;
+    lv_obj_set_style_arc_color(arc, c, LV_PART_INDICATOR);
+}
+
+void av_tick_create(lv_obj_t *parent, lv_coord_t x, lv_coord_t y, int angle, lv_color_t color)
+{
+    lv_point_precise_t points[2] = {{.x = 48, .y = 0}, {.x = 56, .y = 0}};
+    lv_obj_t *tick = lv_line_create(parent);
+    lv_line_set_points(tick, points, 2);
+    lv_obj_set_style_line_width(tick, 2, 0);
+    lv_obj_set_style_line_color(tick, color, 0);
+    lv_obj_set_style_line_rounded(tick, false, 0);
+    lv_obj_set_pos(tick, x + 58, y + 58);
+    lv_obj_set_style_transform_rotation(tick, angle * 10, 0);
 }
 
 void av_define_rpm_widget(lv_obj_t *parent, lv_coord_t x, lv_coord_t y, lv_subject_t *pSubject)
@@ -37,37 +43,18 @@ void av_define_rpm_widget(lv_obj_t *parent, lv_coord_t x, lv_coord_t y, lv_subje
     lv_arc_bind_value(arc, pSubject);
     lv_subject_add_observer(pSubject, av_bind_color, arc);
 
-    // and a label bottom right
     lv_obj_t *label = lv_label_create(parent);
-    lv_obj_set_pos(label, x + 50, y + 70);
+    lv_obj_set_pos(label, x + 64, y + 70);
+    lv_obj_set_size(label, 40, 20);
+    lv_obj_set_style_text_font(label, &Roboto, 0);
+    lv_obj_set_style_text_align(label, LV_TEXT_ALIGN_RIGHT, 0);
+    lv_obj_set_style_text_color(label, WHITE, 0);
     lv_label_bind_text(label, pSubject, "%.0f");
 
-    lv_point_precise_t points[2] = {{.x = 48, .y = 0}, {.x = 56, .y = 0}};
-    lv_obj_t *tick;
+    av_tick_create(parent, x, y, 340, AMBER);
+    av_tick_create(parent, x, y, 350, RED);
     for (int angle = 135; angle <= 360; angle += 45)
     {
-        tick = lv_line_create(parent);
-        lv_line_set_points(tick, points, 2);
-        lv_obj_set_style_line_width(tick, 2, 0);
-        lv_obj_set_style_line_color(tick, lv_color_white(), 0);
-        lv_obj_set_style_line_rounded(tick, false, 0);
-        lv_obj_set_pos(tick, x + 58, y + 58);
-        lv_obj_set_style_transform_rotation(tick, angle * 10, 0);
+        av_tick_create(parent, x, y, angle, WHITE);
     }
-
-    tick = lv_line_create(parent);
-    lv_line_set_points(tick, points, 2);
-    lv_obj_set_style_line_width(tick, 2, 0);
-    lv_obj_set_style_line_color(tick, lv_color_hex3(0xFF0), 0);
-    lv_obj_set_style_line_rounded(tick, false, 0);
-    lv_obj_set_pos(tick, x + 58, y + 58);
-    lv_obj_set_style_transform_rotation(tick, 340 * 10, 0);
-
-    tick = lv_line_create(parent);
-    lv_line_set_points(tick, points, 2);
-    lv_obj_set_style_line_width(tick, 2, 0);
-    lv_obj_set_style_line_color(tick, lv_color_hex3(0xF00), 0);
-    lv_obj_set_style_line_rounded(tick, false, 0);
-    lv_obj_set_pos(tick, x + 58, y + 58);
-    lv_obj_set_style_transform_rotation(tick, 350 * 10, 0);
 }
